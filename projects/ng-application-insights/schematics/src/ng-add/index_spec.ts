6fc94fb2199bba73c0aacd8182dbb39e86b903db
Bernardo Costa Nascimento <bernardocosta@wizsolucoes.com.br>
2020-10-27 14:26:30 -0300

    feat(schematics): ng-application-insights schematics adicionado ao projeto

---
+++ projects/ng-application-insights/schematics/src/ng-add/index_spec.ts
@@ -0,0 +1,16 @@
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+
+describe('ng-application-insights-schematic', () => {
+  it('works', async () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await runner.runSchematicAsync('ng-application-insights-schematic', {}, Tree.empty()).toPromise();
+
+    expect(tree.files).toEqual([]);
+  });
+});
